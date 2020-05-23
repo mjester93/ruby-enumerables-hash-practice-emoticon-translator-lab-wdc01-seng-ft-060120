20faa8c51e0f17f7ae9649517df1320307af220e
Michael Jester <m.jester.93@gmail.com>
2020-05-23 05:38:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_english_meaning(file_path, emoticon)
   emotes = load_library(file_path)
 
   emotes.each do |key|
-    binding.pry
+    puts emotes[key]
     if emotes[key][:japenese] == emoticon
       return key
     end
