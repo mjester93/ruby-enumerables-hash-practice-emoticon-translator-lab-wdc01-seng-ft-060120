3512102ad45a54aa1d4de1a87117ab63280994a6
Michael Jester <m.jester.93@gmail.com>
2020-05-23 05:51:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
     end
   end
 
-  return "Sorry"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
