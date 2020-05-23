4a1d690f28272925ab8f80a19d2a6bc148c42251
Michael Jester <m.jester.93@gmail.com>
2020-05-23 05:22:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,9 @@
 # require modules here
 require 'yaml'
-emoticons = YAML.load_file('lib/emoticons.yml')
 
 def load_library
   # code goes here
+  emoticons = YAML.load_file('lib/emoticons.yml')
 end
 
 def get_japanese_emoticon
